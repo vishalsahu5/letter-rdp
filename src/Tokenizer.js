535ac6656aece144da952b0a275e0c66640b2acc
Sarthak Sahu <vishalsahu5@gmail.com>
2024-12-08 06:26:50 +0100

    Make tokenizer spec based

--- src/Tokenizer.js
+++ src/Tokenizer.js
@@ -1,3 +1,16 @@
+
+/**
+ * Tokenizer Spec.
+ */
+const Spec = [
+    // Numbers:
+    [/^\d+/, 'NUMBER'],
+
+    // Strings:
+    [/^"[^"]*"/, 'STRING'],
+    [/^'[^']*'/, 'STRING'],
+];
+
 /**
  * Tokenizer class.
  * Lazily pulls a token from a stream.
@@ -40,32 +53,30 @@ class Tokenizer {
         }
 
         const string  = this._string.slice(this._cursor);
+        for (const [regexp, tokenType] of Spec) {
+            const tokenValue = this._match(regexp, string);
 
-        //Numbers:
-        if (!Number.isNaN(Number(string[0]))) {
-            let number = '';
-
-            while (!Number.isNaN(Number(string[this._cursor]))) {
-                number += string[this._cursor++];
+            if (tokenValue != null) {
+                return {
+                    type: tokenType,
+                    value: tokenValue,
+                };
             }
-            return {
-                type: 'NUMBER',
-                value: number,
-            };
         }
 
-        //String:
-        if (string[0] === '"') {
-            let s = '';
-            do {
-                s += string[this._cursor++];
-            } while (string[this._cursor] !== '"' && !this.isEOF());
-            s += string[this._cursor++];
-            return {
-                type: 'STRING', 
-                value: s,
-            };
+        throw new SyntaxError(`Unexpected token: ${string[0]}`);
+    }
+
+    /**
+     * Matches a token for a given regular expression.
+     */
+    _match(regexp, string) {
+        const matched = regexp.exec(string);
+        if (matched == null) {
+            return null;
         }
+        this._cursor += matched[0].length;
+        return matched[0];
     }
 }
 
